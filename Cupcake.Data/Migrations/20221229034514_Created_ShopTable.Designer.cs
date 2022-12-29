5cc5e04ce597444613b817f3841b78b321d66d07
jamiecoakley <jamiemcoakley@gmail.com>
2022-12-28 22:48:18 -0500

    Added to Shop table

--- Cupcake.Data/Migrations/20221229034514_Created_ShopTable.Designer.cs
+++ Cupcake.Data/Migrations/20221229034514_Created_ShopTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Cupcake.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221229034514_Created_ShopTable")]
+    partial class CreatedShopTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
